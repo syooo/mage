d4b47bebfaa1554ae5f2e87304a2a62ec69ebb84
syooo <syooo@mail.ru>
2014-01-23 01:32:14 +0400

    Updated grid to bootstrap 3

--- mage-grid.tpl.php
+++ mage-grid.tpl.php
@@ -9,16 +9,16 @@
 
 <div id="yashare" class="yashare-auto-init" data-yashareL10n="ru" data-yashareType="none" data-yashareQuickServices="vkontakte,facebook,twitter,odnoklassniki,moimir,lj,gplus"></div>
 
-<ul class="thumbnails">	
-	<?php foreach ($rows as $row): ?>
-		<li class="span3">
-			<div class="thumbnail">
-				<?php print($row['img']); ?>
-				<div class="caption">
-					<h2><?php print($row['title']); ?></h2>
-					<p><?php /*print($row['body_value']);*/ ?></p> 
-				</div>
-			</div>
-		</li>
-	<?php endforeach; ?>
-</ul>
+<div class="row">
+    <?php foreach ($rows as $row): ?>
+    <div class="col-sm-6 col-md-4">
+        <div class="thumbnail">
+            <?php print($row['img']); ?>
+            <div class="caption">
+                <h3><?php print($row['title']); ?></h3>
+                <p><?php print($row['body_value']); ?></p>
+            </div>
+        </div>
+    </div>
+    <?php endforeach; ?>
+</div>
